9a51c6278ee9a11fffc6aba0408ba256f2be5073
Shivam-Koushik <93977658+ShivamKoushik@users.noreply.github.com>
2022-06-04 00:02:22 +0530

    get Api

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,6 +36,41 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+router.get('/movies', function(req, res){
+    const arr = ["RRR","Runway34","GanguBai","heropanti2","Attack"]
+    res.send(arr)
+  
+})
+
+
+router.get('/movies/:indexNumber', function(req, res){
+    const arr = ["RRR","Runway34","GanguBai","heropanti2","Attack"]
+    if(req.params.indexNumber<arr.length){
+    console.log("Selected index is :" + arr[req.params.indexNumber])
+    res.send(arr[req.params.indexNumber])
+    }else{
+        console.log("Not a valid number")
+    }
+})
+
+
+router.get('/films', function(req, res){
+   const arr1 =[{"id" : 1, "name" : "RRR"},{"id" : 2, "name" : "Runway34"},{"id" : 3, "name" : "GanguBai"}]
+   res.send(arr1)
+
+})
+
+
+router.get('/films/:filmId', function(req, res){
+   const arr1 =[{"id" : 0, "name" : "RRR"},{"id" : 1, "name" : "Runway34"},{"id" : 2, "name" : "GanguBai"}]
+   if(req.params.filmId<arr1.length){
+       console.log("selected movie index is : " + arr1[req.params.filmId])
+       res.send(arr1[req.params.filmId])
+   }else{
+       console.group(" not is valid number")
+   }
+
+})
 
 module.exports = router;
 // adding this comment for no reason
