94555d029211c1bdb9e4f7517b58a60807931b23
Shivam-Koushik <93977658+ShivamKoushik@users.noreply.github.com>
2022-06-04 19:17:04 +0530

    missingNum

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,37 @@
 const express = require('express');
-const externalModule = require('./logger')
+const app = express.Router();
 
-const router = express.Router();
+app.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+        
+  app.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
 
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
-});
-
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
-
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
-
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
-
-module.exports = router;
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+module.exports = app;
 // adding this comment for no reason
