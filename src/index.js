60afdf7f0c14e54ff459011178339dc6e20fbb53
Shivam-Koushik <93977658+ShivamKoushik@users.noreply.github.com>
2022-06-10 22:58:28 +0530

    mdg

--- src/index.js
+++ src/index.js
@@ -4,22 +4,35 @@ const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+const middle1 = function(req,res,next){
+    console.log("my first middleware")
+    next()
+}
+
+const middle2 = function(req,res,next){
+      let currentDate = new Date();
+      let dateTime = currentDate.getDate()+"/"+(currentDate.getMonth()+1)+"/"+currentDate.getFullYear()+"   "+
+                     currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds()
+
+      let ip = req.ip
+      let url = req.originalUrl 
+      
+      console.log( dateTime +"   "+ip+"   "+url)
+      next()
+}
+
+app.use(middle1);
+app.use(middle2);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ShivamKoushik:s%40H9663334444@cluster0.k1qkf.mongodb.net/Shivam4545-DB2?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
+.then( () => console.log("Hi Shivam Your MongoDb is connected"))
 .catch ( err => console.log(err) )
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
 
 app.use('/', route);
 
